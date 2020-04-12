ad560e9e051e8276f4522305a71b4c6cdcb0f0e8
Josh Stillman <josh.stillman@giantmachines.com>
2020-04-12 16:14:37 -0400

    logging for info

--- utils/createAssetsJson.ts
+++ utils/createAssetsJson.ts
@@ -7,6 +7,19 @@ const generateJson = () => {
     .readdirSync(path.resolve('public/', 'static/'))
     .filter((image: string) => /jpe?g/.test(image));
 
+  console.log('dir read resolve', path.resolve('public/', 'static/'));
+  console.log('dir read join', path.join('public/', 'static/'));
+  console.log(
+    'dir  join contents',
+    fs.readdirSync(path.resolve('public/', 'static/'))
+  );
+  console.log(
+    'dir join contents',
+    fs.readdirSync(path.join('public/', 'static/'))
+  );
+
+  console.log('top level contents', fs.readdirSync(path.join('public')));
+
   const obj = { pics };
 
   const json = JSON.stringify(obj);
