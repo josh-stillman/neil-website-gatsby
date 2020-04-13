58292911e4e25a80bd1debe415013eb1dcd5f9f9
Josh Stillman <josh.stillman@giantmachines.com>
2020-04-12 23:33:30 -0400

    remove routing

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,12 +6,14 @@
  */
 
 // You can delete this file if you're not using it
-exports.onCreatePage = ({ page, actions }) => {
-  const { createPage } = actions;
-  // Make the front page match everything client side.
-  // Normally your paths should be a bit more judicious.
-  if (page.path === `/`) {
-    page.matchPath = `/*`;
-    createPage(page);
-  }
-};
+
+// client-side routes can be set here.  Example below sets all routes to client routes
+// exports.onCreatePage = ({ page, actions }) => {
+//   const { createPage } = actions;
+//   // Make the front page match everything client side.
+//   // Normally your paths should be a bit more judicious.
+//   if (page.path === `/`) {
+//     page.matchPath = `/*`;
+//     createPage(page);
+//   }
+// };
